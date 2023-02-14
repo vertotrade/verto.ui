d43a20253146ed6ebf11361d19d1dd7829d61806
noam <noamkadosh91@gmail.com>
2023-02-14 09:59:13 -0800

    feat(DEV-500): removing telegram dropdown

--- packages/uikit/src/components/Footer/config.tsx
+++ packages/uikit/src/components/Footer/config.tsx
@@ -1,15 +1,6 @@
 import { Language } from "../LangSelector/types";
 import { FooterLinkType } from "./types";
-import {
-  TwitterIcon,
-  TelegramIcon,
-  // RedditIcon,
-  // InstagramIcon,
-  GithubIcon,
-  // DiscordIcon,
-  // MediumIcon,
-  // YoutubeIcon,
-} from "../Svg";
+import { TwitterIcon, TelegramIcon, GithubIcon } from "../Svg";
 
 export const footerLinks: FooterLinkType[] = [
   {
@@ -86,103 +77,13 @@ export const socials = [
   {
     label: "Telegram",
     icon: TelegramIcon,
-    items: [
-      {
-        label: "English",
-        href: "https://t.me/vertotrade",
-      },
-      {
-        label: "Bahasa Indonesia",
-        href: "https://t.me/VertoTradeIndonesia",
-      },
-      {
-        label: "中文",
-        href: "https://t.me/VertoTrade_CN",
-      },
-      {
-        label: "Tiếng Việt",
-        href: "https://t.me/VertoTradeVN",
-      },
-      {
-        label: "Italiano",
-        href: "https://t.me/vertotrade_ita",
-      },
-      {
-        label: "русский",
-        href: "https://t.me/vertotrade_ru",
-      },
-      {
-        label: "Türkiye",
-        href: "https://t.me/vertotradeturkiye",
-      },
-      {
-        label: "Português",
-        href: "https://t.me/VertoTradePortuguese",
-      },
-      {
-        label: "Español",
-        href: "https://t.me/VertoTradeEs",
-      },
-      {
-        label: "日本語",
-        href: "https://t.me/vertotradejp",
-      },
-      {
-        label: "Français",
-        href: "https://t.me/vertotradefr",
-      },
-      {
-        label: "Deutsch",
-        href: "https://t.me/VertoTrade_DE",
-      },
-      {
-        label: "Filipino",
-        href: "https://t.me/VertoTrade_Ph",
-      },
-      {
-        label: "ქართული ენა",
-        href: "https://t.me/VertoTradeGeorgia",
-      },
-      {
-        label: "हिन्दी",
-        href: "https://t.me/VertoTradeINDIA",
-      },
-      {
-        label: "Announcements",
-        href: "https://t.me/VertoTradeAnn",
-      },
-    ],
+    href: "https://t.me/vertotrade",
   },
   {
     label: "Github",
     icon: GithubIcon,
     href: "https://github.com/vertotrade/",
   },
-  // {
-  //   label: "Reddit",
-  //   icon: RedditIcon,
-  //   href: "https://reddit.com/r/vertotrade",
-  // },
-  // {
-  //   label: "Instagram",
-  //   icon: InstagramIcon,
-  //   href: "https://instagram.com/vertotrade_official",
-  // },
-  // {
-  //   label: "Discord",
-  //   icon: DiscordIcon,
-  //   href: "https://discord.gg/vertotrade",
-  // },
-  // {
-  //   label: "Medium",
-  //   icon: MediumIcon,
-  //   href: "https://medium.com/vertotrade",
-  // },
-  // {
-  //   label: "Youtube",
-  //   icon: YoutubeIcon,
-  //   href: "https://www.youtube.com/@vertotrade_official",
-  // },
 ];
 
 export const langs: Language[] = [...Array(20)].map((_, i) => ({
