0c4dcd231fb5a3388aa48ec241bce5f147db41f7
boogiecyclon <121128135+boogiecyclon@users.noreply.github.com>
2023-02-21 09:29:59 -0800

    Feat/dev 500 (#32)
    * feat(DEV-500): removing telegram dropdown
    
    * feat(DEV-500): social links fix
    
    * feat(DEV-500): social links fix
    
    ---------
    
    Co-authored-by: noam <noamkadosh91@gmail.com>

--- packages/uikit/src/components/Footer/Components/SocialLinks.tsx
+++ packages/uikit/src/components/Footer/Components/SocialLinks.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import styled from "styled-components";
 import { FlexProps } from "../../Box";
 import Flex from "../../Box/Flex";
-import Dropdown from "../../Dropdown/Dropdown";
 import Link from "../../Link/Link";
 import { socials } from "../config";
 
@@ -30,17 +29,7 @@ const SocialLinks: React.FC<React.PropsWithChildren<FlexProps>> = ({ ...props })
       };
       const Icon = social.icon;
       const mr = index < socials.length - 1 ? "24px" : 0;
-      if (social.items) {
-        return (
-          <Dropdown key={social.label} position="top" target={<Icon {...iconProps} mr={mr} mb="-3px" />}>
-            {social.items.map((item) => (
-              <Link external key={item.label} href={item.href} aria-label={item.label} color="textSubtle">
-                {item.label}
-              </Link>
-            ))}
-          </Dropdown>
-        );
-      }
+
       return (
         <SocialLink external key={social.label} href={social.href} aria-label={social.label} mr={mr}>
           <Icon {...iconProps} />
