42a71aa47f3a46252e5251852eda4d57be727c85
ludapops <goheat631@gmail.com>
2023-03-03 12:39:25 -0500

    feat(Dev-535): add back nft code and comment mentions of profiles

---
+++ apps/web/src/views/Nft/market/components/CollectibleCard/CollectionCardWithVolume.tsx
@@ -0,0 +1,35 @@
+import { Flex, Text } from '@verto/uikit'
+import { useTranslation } from '@verto/localization'
+import { BNBAmountLabel } from './styles'
+import { CollectionCard } from './index'
+
+interface CollectionCardWithVolumeProps {
+  bgSrc: string
+  avatarSrc?: string
+  collectionName: string
+  url?: string
+  disabled?: boolean
+  volume: number
+}
+
+const CollectionCardWithVolume: React.FC<CollectionCardWithVolumeProps> = ({
+  bgSrc,
+  avatarSrc,
+  collectionName,
+  url,
+  volume,
+}) => {
+  const { t } = useTranslation()
+  return (
+    <CollectionCard bgSrc={bgSrc} avatarSrc={avatarSrc} collectionName={collectionName} url={url}>
+      <Flex alignItems="center">
+        <Text fontSize="12px" color="textSubtle">
+          {t('Volume')}
+        </Text>
+        <BNBAmountLabel amount={volume} />
+      </Flex>
+    </CollectionCard>
+  )
+}
+
+export default CollectionCardWithVolume
