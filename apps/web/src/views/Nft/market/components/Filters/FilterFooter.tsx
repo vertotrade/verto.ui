fdd7c2866b8214a97362f9413f691a6ad830bf51
ludapops <goheat631@gmail.com>
2023-03-03 12:39:25 -0500

    feat(Dev-535): add back nft code and comment mentions of profiles

---
+++ apps/web/src/views/Nft/market/components/Filters/FilterFooter.tsx
@@ -0,0 +1,16 @@
+import { Grid, GridProps } from '@verto/uikit'
+
+const FilterFooter: React.FC<React.PropsWithChildren<GridProps>> = ({ children, ...props }) => (
+  <Grid
+    gridGap="16px"
+    gridTemplateColumns="repeat(2,1fr)"
+    {...props}
+    px="24px"
+    py="16px"
+    borderTop="1px solid"
+    borderTopColor="cardBorder">
+    {children}
+  </Grid>
+)
+
+export default FilterFooter
