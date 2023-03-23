270dc316a66bf2bf1d02f3977a3d7fd9668eb904
ludapops <goheat631@gmail.com>
2023-03-23 13:22:44 -0400

    feat(Dev-555): update text to minting pools

--- apps/web/src/views/Home/components/FarmsPoolsRow/index.tsx
+++ apps/web/src/views/Home/components/FarmsPoolsRow/index.tsx
@@ -69,7 +69,7 @@ const FarmsPoolsRow = () => {
     <div ref={observerRef}>
       <Flex flexDirection="column" mt="24px">
         <Flex mb="24px">
-          <RowHeading text={showFarms ? t('Top Farms') : t('Top Res Pools')} />
+          <RowHeading text={showFarms ? t('Top Farms') : t('Top Minting Pools')} />
           {chainId === ChainId.BSC && (
             <IconButton
               variant="text"
