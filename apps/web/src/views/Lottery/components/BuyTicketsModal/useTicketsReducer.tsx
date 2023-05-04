6ac127337c4149da90073986cd8b415ecd475bfa
ludapops <goheat631@gmail.com>
2023-05-04 10:10:32 -0400

    feat(Dev-575): fix linter

--- apps/web/src/views/Lottery/components/BuyTicketsModal/useTicketsReducer.tsx
+++ apps/web/src/views/Lottery/components/BuyTicketsModal/useTicketsReducer.tsx
@@ -23,8 +23,8 @@ const getInitialState = ({
   userCurrentTickets: LotteryTicket[]
 }): TicketsState => {
   const randomTickets = generateTicketNumbers(amount, userCurrentTickets)
-  const randomTicketsAsStringArray = randomTickets.map((ticket) => parseRetrievedNumber(ticket.toString()).split(''))
-  const tickets = Array.from({ length: amount }, (_, i) => i + 1).map((index) => ({
+  const randomTicketsAsStringArray = randomTickets.map(ticket => parseRetrievedNumber(ticket.toString()).split(''))
+  const tickets = Array.from({ length: amount }, (_, i) => i + 1).map(index => ({
     id: index,
     numbers: randomTicketsAsStringArray[index - 1],
     duplicateWith: [],
@@ -43,17 +43,17 @@ const reducer = (state: TicketsState, action: any) => {
       const { ticketId, newNumbers } = action.payload
 
       const newDuplicates = state.tickets.filter(
-        (ticket) => ticket.id !== ticketId && ticket.isComplete && ticket.numbers.join('') === newNumbers.join(''),
+        ticket => ticket.id !== ticketId && ticket.isComplete && ticket.numbers.join('') === newNumbers.join(''),
       )
 
       // If ticket was duplicate but not duplicate anymore with this update
       // go through previously considered duplicates and remove id of this ticket
       // from their duplicateWith array
       const prevDuplicates = tickets[ticketId - 1].duplicateWith
-      prevDuplicates.forEach((prevTicketId) => {
+      prevDuplicates.forEach(prevTicketId => {
         if (!newDuplicates.map(({ id }) => id).includes(prevTicketId)) {
           const dupsToUpdate = [...tickets[prevTicketId - 1].duplicateWith]
-          const indexToRemove = dupsToUpdate.findIndex((id) => id === ticketId)
+          const indexToRemove = dupsToUpdate.findIndex(id => id === ticketId)
           dupsToUpdate.splice(indexToRemove, 1)
           tickets[prevTicketId - 1] = {
             ...tickets[prevTicketId - 1],
@@ -64,7 +64,7 @@ const reducer = (state: TicketsState, action: any) => {
 
       // If found duplicates - update their duplicateWith array
       if (newDuplicates.length !== 0) {
-        newDuplicates.forEach((duplicate) => {
+        newDuplicates.forEach(duplicate => {
           tickets[duplicate.id - 1] = {
             ...duplicate,
             duplicateWith: [...duplicate.duplicateWith, ticketId],
@@ -75,13 +75,13 @@ const reducer = (state: TicketsState, action: any) => {
       const updatedTicket = {
         id: ticketId,
         numbers: newNumbers,
-        duplicateWith: newDuplicates.map((ticket) => ticket.id),
+        duplicateWith: newDuplicates.map(ticket => ticket.id),
         isComplete: newNumbers.join('').length === 6,
       }
       tickets[ticketId - 1] = updatedTicket
 
       // Check if all tickets are filled
-      const allComplete = tickets.every((ticket) => ticket.isComplete)
+      const allComplete = tickets.every(ticket => ticket.isComplete)
 
       return {
         tickets,
@@ -116,8 +116,8 @@ export const useTicketsReducer = (
   }
 
   const getTicketsForPurchase = () => {
-    return state.tickets.map((ticket) => {
-      const reversedTicket = [...ticket.numbers].map((num) => parseInt(num, 10)).reverse()
+    return state.tickets.map(ticket => {
+      const reversedTicket = [...ticket.numbers].map(num => parseInt(num, 10)).reverse()
       reversedTicket.unshift(1)
       const ticketAsNumber = parseInt(reversedTicket.join(''), 10)
       return ticketAsNumber
