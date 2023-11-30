2d668d3006871f789a197ff6674252746e3c54f1
phoenix377 <phoenix.number377@gmail.com>
2023-11-30 07:29:27 -0800

    DEV-741 Change wallet modal min-width

--- apps/web/src/components/Menu/UserMenu/WalletModal.tsx
+++ apps/web/src/components/Menu/UserMenu/WalletModal.tsx
@@ -74,7 +74,7 @@ const WalletModal: React.FC<React.PropsWithChildren<WalletModalProps>> = ({
   }, [])
 
   return (
-    <ModalContainer title={t('Welcome!')} $minWidth="360px">
+    <ModalContainer title={t('Welcome!')} $minWidth="480px">
       <ModalHeader>
         <ModalTitle>
           <Heading>{t('Your Wallet')}</Heading>
