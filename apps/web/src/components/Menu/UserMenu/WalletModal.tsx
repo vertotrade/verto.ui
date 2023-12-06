73752a78b8107694afd4c0d1de3138dabe17cbd1
phoenix377 <phoenix.number377@gmail.com>
2023-12-06 10:21:30 -0800

    DEV-754 Fix modal top

--- apps/web/src/components/Menu/UserMenu/WalletModal.tsx
+++ apps/web/src/components/Menu/UserMenu/WalletModal.tsx
@@ -74,7 +74,7 @@ const WalletModal: React.FC<React.PropsWithChildren<WalletModalProps>> = ({
   }, [])
 
   return (
-    <ModalContainer title={t('Welcome!')} $minWidth="360px">
+    <ModalContainer title={t('Welcome!')} $minWidth="360px" top="200px">
       <ModalHeader>
         <ModalTitle>
           <Heading>{t('Your Wallet')}</Heading>
