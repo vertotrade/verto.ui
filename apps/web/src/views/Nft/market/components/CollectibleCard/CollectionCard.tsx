b9afdd514b3694d84ce9e19010eedcbbd5784a5f
ilia-mh <iliya.numb@gmail.com>
2023-12-18 19:19:18 -0500

    applied new design edits to newest collections section and collection cards

--- apps/web/src/views/Nft/market/components/CollectibleCard/CollectionCard.tsx
+++ apps/web/src/views/Nft/market/components/CollectibleCard/CollectionCard.tsx
@@ -1,12 +1,15 @@
-import { Card, CardBody, Flex, Heading, ProfileAvatar, NextLinkFromReactRouter } from '@verto/uikit'
-
+import { Card, CardBody, Flex, Heading, Text, NextLinkFromReactRouter, ProfileAvatar } from '@verto/uikit'
+import { useTranslation } from '@verto/localization'
+import useTheme from 'hooks/useTheme'
 import Image from 'next/image'
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 
 interface CollectionCardProps {
   bgSrc: string
   avatarSrc?: string
   collectionName: string
+  collectionVolume?: string
+  collectionSupply?: string
   url?: string
   disabled?: boolean
 }
@@ -19,68 +22,98 @@ export const CollectionAvatar = styled(ProfileAvatar)`
 `
 
 const StyledCollectionCard = styled(Card)<{ disabled?: boolean }>`
-  border-radius: 20px;
-  border-bottom-left-radius: 56px;
-  transition: opacity 200ms;
+  border-radius: 16px;
   height: 100%;
   max-width: 375px;
   margin: 0 auto;
+  background: transparent;
+  width: 100%;
 
   & > div {
     border: 1px solid ${({ theme }) => theme.colors.cardBorder};
-    border-radius: 20px;
-    border-bottom-left-radius: 56px;
-  }
-
-  ${({ theme }) => theme.mediaQueries.lg} {
-    ${({ disabled }) =>
-      disabled
-        ? ''
-        : css`
-            &:hover {
-              cursor: pointer;
-              opacity: 0.6;
-            }
-          `}
+    border-radius: 16px;
+    padding: 8px;
+    width: 100%;
   }
 `
 
 const StyledImage = styled(Image)`
-  height: 115px;
-  width: 375px;
+  width: 100%;
+  aspect-ratio: 2.7/1;
+  object-fit: cover;
+  border-radius: 8px;
+  margin-bottom: 16px;
+`
+
+const StyledCardDetailsContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  & > div {
+    width: 50%;
+  }
 `
 
 const CollectionCard: React.FC<React.PropsWithChildren<CollectionCardProps>> = ({
   bgSrc,
   avatarSrc,
   collectionName,
+  collectionVolume,
+  collectionSupply,
   url,
   disabled,
   children,
 }) => {
+  const { t } = useTranslation()
+  const { isDark } = useTheme()
+
   const renderBody = () => (
     <CardBody p="0">
       <StyledImage src={bgSrc} alt={`nft-collection-card-${collectionName}`} height={125} width={375} />
-      <Flex
-        position="relative"
-        height="65px"
-        justifyContent="center"
-        alignItems="flex-end"
-        pt="8px"
-        mb="12px"
-        mx="12px"
-        flexDirection="column">
-        <CollectionAvatar src={avatarSrc} width={96} height={96} />
-        <Heading color={disabled ? 'textDisabled' : 'body'} as="h3" mb={children ? '8px' : '0'}>
+      <Flex position="relative" justifyContent="center" pt="8px" mb="8px" mx="12px" flexDirection="column">
+        {avatarSrc ? <CollectionAvatar src={avatarSrc} width={96} height={96} /> : ''}
+        <Heading
+          color={disabled ? 'textDisabled' : 'body'}
+          scale="h4"
+          as="h3"
+          mb={collectionSupply || collectionVolume ? '16px' : '0'}>
           {collectionName}
         </Heading>
+
+        {collectionSupply || collectionVolume ? (
+          <StyledCardDetailsContainer>
+            {collectionVolume ? (
+              <div>
+                <Text small color={isDark ? '#818181' : '#969EAD'}>
+                  {t('Volume')}
+                </Text>
+                <Text>{collectionVolume}</Text>
+              </div>
+            ) : (
+              ''
+            )}
+            {collectionSupply ? (
+              <div>
+                <Text small color={isDark ? '#818181' : '#969EAD'}>
+                  {t('Items')}
+                </Text>
+                <Text>{collectionSupply}</Text>
+              </div>
+            ) : (
+              ''
+            )}
+          </StyledCardDetailsContainer>
+        ) : (
+          ''
+        )}
+
         {children}
       </Flex>
     </CardBody>
   )
 
   return (
-    <StyledCollectionCard disabled={disabled} data-test="hot-collection-card">
+    <StyledCollectionCard disabled={disabled} data-test="hot-collection-card" background="transparent">
       {url ? (
         <NextLinkFromReactRouter to={url}>{renderBody()}</NextLinkFromReactRouter>
       ) : (
