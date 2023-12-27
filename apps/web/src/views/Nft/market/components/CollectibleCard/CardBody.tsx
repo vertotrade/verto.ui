02d9ac8f4d581ba922d40c3dd79536f265075637
Diana <sahandianaa@gmail.com>
2023-12-26 17:45:26 -0800

    DEV-770: Update nft Collection Cards

--- apps/web/src/views/Nft/market/components/CollectibleCard/CardBody.tsx
+++ apps/web/src/views/Nft/market/components/CollectibleCard/CardBody.tsx
@@ -18,20 +18,15 @@ const CollectibleCardBody: React.FC<React.PropsWithChildren<CollectibleCardProps
   const [token, tokenPrice] = useTokenAndPriceByAddress(nft.marketData?.currency)
 
   return (
-    <CardBody p="0" style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+    <CardBody p="0">
       <NFTMedia as={PreviewImage} nft={nft} height={320} width={320} mb="12px" />
       <Flex alignItems="center" justifyContent="space-between" px="12px" mt="auto">
-        {nft?.collectionName && (
-          <Text fontSize="12px" color="textSubtle">
-            {nft?.collectionName}
-          </Text>
-        )}
         {nftLocation && <LocationTag nftLocation={nftLocation} />}
       </Flex>
-      <Text as="h4" fontWeight="600" mb="12px" px="12px">
+      <Text as="h4" fontWeight="600" mb="16px" px="12px">
         {name}
       </Text>
-      <Box borderTop="1px solid" borderTopColor="cardBorder" p="12px" mt="auto">
+      <Box px="12px" mt="auto">
         {currentAskPrice && (
           <MetaRow title={isUserNft ? t('Your price') : t('Asking price')}>
             <CostLabel cost={currentAskPrice} usdPrice={tokenPrice} token={token} />
