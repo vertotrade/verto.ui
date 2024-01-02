eecae9f28ae18bea2779ebdd466c0f0f963ebab8
Diana Sahan <sahandianaa@gmail.com>
2024-01-02 10:44:09 -0800

    DEV-769: Update NFT Collection Filters UI (#209)
    * DEV-769: Update NFT Collection Filters UI

--- packages/uikit/src/components/ButtonMenu/ButtonMenuItem.tsx
+++ packages/uikit/src/components/ButtonMenu/ButtonMenuItem.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { PolymorphicComponent } from "../../util/polymorphic";
 import Button from "../Button/Button";
-import { BaseButtonProps } from "../Button/types";
+import { BaseButtonProps, variants } from "../Button/types";
 import { ButtonMenuItemProps } from "./types";
 
 interface MaybeActiveButtonProps extends BaseButtonProps {
@@ -12,20 +12,23 @@ interface MaybeActiveButtonProps extends BaseButtonProps {
 const MaybeActiveButton: PolymorphicComponent<MaybeActiveButtonProps, "button"> = styled(
   Button
 )<MaybeActiveButtonProps>`
-  background-color: ${({ theme, isActive, isPrimary, noBg }) => {
+  background-color: ${({ theme, isActive, isPrimary, variant, noBg }) => {
     if ( !isActive && noBg ) return "transparent"
+    if (variant === variants.LIGHT) return isActive ? theme.colors.buttonMenuActiveBg : theme.colors.buttonMenuBg;
     else if (isPrimary) return isActive ? theme.colors.newPrimary : theme.colors.inputBg;
     return isActive ? theme.colors.backgroundAlt : theme.colors.inputBg;
   }};
-  color: ${({ theme, isActive, isPrimary, isDark }) => {
+  color: ${({ theme, isActive, isPrimary, isDark, variant }) => {
     if (isPrimary) return isActive ? (isDark ? "black" : "white") : theme.colors.disabledTextDark;
+    if (variant === variants.LIGHT) return isActive ? theme.colors.text : theme.colors.disbaledText;
     return isActive ? theme.colors.text : theme.colors.disabledTextDark;
   }};
   border-radius: 4px;
 
   &:hover:not(:disabled):not(:active) {
-    background-color: ${({ theme, isActive, isPrimary }) => {
+    background-color: ${({ theme, isActive, isPrimary, variant }) => {
       if (isPrimary) return isActive ? theme.colors.newPrimaryDark : "transparent";
+      if (variant === variants.LIGHT) return theme.colors.buttonMenuActiveBg;
       return isActive ? theme.colors.backgroundAlt : "transparent";
     }};
     color: ${({ theme, isActive, isPrimary, isDark }) => {
